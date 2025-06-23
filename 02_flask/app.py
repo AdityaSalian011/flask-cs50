915d17e9eaa65d922e57fc339b756122828fa4d1
Aditya Salian <adityasalian865@gmail.com>
2025-06-23 12:28:04 +0000

    flask part 2

--- 02_flask/app.py
+++ 02_flask/app.py
@@ -3,19 +3,28 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
 
 # @app.route('/greet')
 # def greet():
 #     name = request.args.get('name', 'world')
 #     return render_template('greet.html', name=name)
 
-@app.route('/greet', methods=['POST'])
-def greet():
-    name = request.form.get('name', 'world')
-    return render_template('greet.html', name=name)
+# @app.route('/greet', methods=['POST'])
+# def greet():
+#     name = request.form.get('name', 'world')
+#     return render_template('greet.html', name=name)
+
+# combining home and greet page
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # return render_template('greet.html', name=request.form.get('name', 'world'))
+        return render_template('greet.html', name=request.form.get('name'))
+    else:
+        return render_template('index.html')
 
 if __name__=='__main__':
     app.run(debug=True)
